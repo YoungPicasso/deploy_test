43b24e8815a5611e080562439480418c5253b20a
Jovanni <jovannitjonasam@outlook.com>
2026-01-06 20:55:56 +0100

    update next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Ensure Next/Turbopack uses this folder as the workspace root
+  turbopack: {
+    root: process.cwd(),
+  },
 };
 
 export default nextConfig;
